26d020b9e6c12528bcd9439f9beac6f0b54fb00f
ntpten-x <natthaphongcontact@gmail.com>
2026-06-30 11:32:40 +0700

    edit part

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -261,7 +261,7 @@ export default function Header() {
   const handleLogout = () => {
     logout(null, {
       onSuccess: () => {
-        navigate('/login');
+        navigate('/');
       }
     });
   };
